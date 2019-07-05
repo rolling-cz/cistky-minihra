323e03ef0bdb4254945c18e8d3900a08d30c3dde
Tomas Fechtner <fechtnertomas@gmail.com>
2019-07-05 14:57:39 +0200

    initial structure

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
